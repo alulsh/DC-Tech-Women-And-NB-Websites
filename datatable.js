d5656abff59a0f7eec05ece2cbb92bd644804790
Alexandra Ulsh <ulsh@mapbox.com>
2017-12-24 11:49:11 -0500

    Switches to two spaces to make code easier to read

--- datatable.js
+++ datatable.js
@@ -1,29 +1,21 @@
 $(document).ready(function() {
-    
-    $('#dataTable').DataTable( {
-        "ajax": 'websites.json',
-        "order": [[0, "asc"]],
-        "lengthMenu": [10, 25, 50, 75, 100],
-        "columnDefs": [
-                {
-                    "render": function (data, type, row) {
-                        
-                        var link = convertToLink(data);
-                        
-                        return link;
-                        
-                    },
-                    "targets": 1
-                }
-            ]
-    } );
-    
-} );
+  $('#dataTable').DataTable( {
+    'ajax': 'websites.json',
+    'order': [[0, 'asc']],
+    'lengthMenu': [10, 25, 50, 75, 100],
+    'columnDefs': [
+        {
+          'render': function (data, type, row) {
+            var link = convertToLink(data);
+            return link;
+          },
+          'targets': 1
+        }
+      ]
+  });
+});
 
 function convertToLink(link) {
-    
-    var html = '<a href="' + link + '">' + link + "</a>";
-    
-    return html;
-    
+  var html = '<a href="' + link + '">' + link + "</a>";
+  return html;
 }
